a8ecfdcba6c631d4b481f1645239a0f5e974e81f
Suriya <suriyag1999@gmail.com>
2024-03-29 18:57:15 +0530

    version change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='podman-init',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     include_package_data=True,
